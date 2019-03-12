33b4bf6962e5037a661dc74081051e40e1df1a89
Brian <brian.yimtam@student.manchester.ac.uk>
2019-03-12 19:12:50 +0000

    trying to actually remove redundant files

--- database/Queries/createBets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Bets (
-    bet_id char(10) NOT NULL,
-    exam_id char(10) NOT NULL,
-    user_id char(10) NOT NULL,
-    target_id char(10) NOT NULL,
-    guess_mark tinyint,
-    win bit,
-    PRIMARY KEY(bet_id)
-);
--- database/Queries/createGroups.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Groups (
-    group_id char(10) NOT NULL,
-    group_name varchar(20),
-    user_id char(10) NOT NULL,
-    credits int NOT NULL
-);
