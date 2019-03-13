55e8704c06eef3a23ee51e9e198924bb9a8e4016
Kamil Synak <kamil.synak@student.manchester.ac.uk>
2019-03-13 18:14:39 +0000

    Models created and syncdb initiated

--- GradeMate/settings.py
+++ GradeMate/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'mainsite',
     'loginsystem',
+<<<<<<< HEAD
     'game',
+=======
+    'game'
+>>>>>>> Models created and syncdb initiated
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ game/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class User(models.Model):
+    user_id = models.CharField('User ID', max_length=10, primary_key=True)
+    user_name = models.CharField('User Name', max_length=20)
+
+    def __unicode__(self):
+        return self.user_name
+
+class Group(models.Model):
+    group_id = models.CharField('Group ID', max_length=10, primary_key=True)
+    group_name = models.CharField('Group Name', max_length=20)
+
+    def __unicode__(self):
+        return self.group_name
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group)
+    user = models.ForeignKey(User)
+    credits = models.PositiveIntegerField('Group Credits')
+
+    def __unicode__(self):
+        return self.user
+
+class Exam(models.Model):
+    exam_id = models.CharField('Exam ID', max_length=10, primary_key=True)
+    group = models.ForeignKey(Group)
+
+    def __unicode__(self):
+        return self.exam_id
+
+class Bet(models.Model):
+    bet_id = models.CharField('Bet ID', max_length=10, primary_key=True)
+    exam = models.ForeignKey(Exam)
+    user = models.ForeignKey(User, related_name='creator')
+    target = models.ForeignKey(User, related_name='target')
+    guess_mark = models.PositiveIntegerField('Guessed Mark')
+    win = models.NullBooleanField('Winning Bet', default=None, blank=True)
+
+    def __unicode__(self):
+        return self.bet_id
+
+class Result(models.Model):
+    exam = models.ForeignKey(Exam)
+    user = models.ForeignKey(User)
+    mark = models.PositiveIntegerField('Mark')
+
+    def __unicode__(self):
+        return self.mark
