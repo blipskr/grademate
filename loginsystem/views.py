63e76438f98fb1c0300579def8839ad0fb7729ab
Robert-Matyas Gyorfi <robert-matyas.gyorfi@student.manchester.ac.uk>
2019-03-18 13:13:08 +0000

    Resolved merge conflict in loginsystem

--- loginsystem/views.py
+++ loginsystem/views.py
@@ -5,11 +5,22 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-import re
+from game.models import Bet, GroupMember, Group, Exam, Result
 
 minPasswordLength = 7
-
 # Create your views here.
+
+def validatePassword(password):
+    if (len(password) < minPasswordLength):
+        return False
+    elif (re.findall('[A-Z]', password)):
+        return True
+    elif (re.findall('[a-z]', password)):
+        return True
+    elif (re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password)):
+        return True
+    else:
+        return False
 
 def register_view(request):
     if request.method == 'POST':
@@ -54,9 +65,6 @@ def logout_view(request):
 def profile_view(request):
     return render(request, 'profile.html',)
 
-@login_required(login_url="/login/")
-def statistics_view(request):
-    return render(request, 'statistics.html')
 
 @login_required(login_url="/login/")
 def accountsettings_view(request):
@@ -84,14 +92,19 @@ def settingsautherror_view(request, form):
 def settingsbadfielderror_view(request, form):
     render(request, 'accountsettingswrongfielderror.html', { 'form': form })
 
-def validatePassword(password):
-    if (len(password) < minPasswordLength):
-        return False
-    elif (re.findall('[A-Z]', password)):
-        return True
-    elif (re.findall('[a-z]', password)):
-        return True
-    elif (re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password)):
-        return True
-    else:
-        return False
+
+@login_required(login_url="/login/")
+def statistics_view(request):
+    #get the users name
+    user = request.user.username
+    #get the users id
+    data = User.objects.filter(username=user)
+    # get the users groups
+    groups = GroupMember.objects.filter(user_id = data[0].id)
+    # get the group name
+    #groupName = Group.objects.filter(group_id in groups)
+    student_lib = {
+    "name": user,
+    "groups" : groups
+    }
+    return render(request, 'statistics.html', student_lib)
