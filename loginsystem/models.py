369dbfcb86918dca3581fee5aaa139df9fdab561
Robert-Matyas Gyorfi <robert-matyas.gyorfi@student.manchester.ac.uk>
2019-03-18 13:01:21 +0000

    Statistic page progress2

--- loginsystem/models.py
+++ loginsystem/models.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.models import User
 from game.models import Bet, GroupMember, Group, Exam, Result
+<<<<<<< HEAD
 
+=======
+#user = request.user.username
+#data = Bet.objects.all()
+#student_lib = {
+#"student_number": data
+#}
+>>>>>>> Statistic page progress2
 # Create your models here.
