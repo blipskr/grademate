e9566965a41dfe70c63b0fd3dd4fdfeff655abf9
Danielius Viktorovas <danielius.viktorovas@student.manchester.ac.uk>
2019-03-19 14:49:55 +0000

    Updated ExamStats.py

--- game/ExamStats.py
+++ game/ExamStats.py
@@ -2,6 +2,7 @@ class ExamStats:
 
     def __init__(self, exam_id, average_bet, no_of_bets, id, exam_name):
         self.exam_id = exam_id
+        self.exam = exam_id
         self.average_bet = average_bet
         self.no_of_bets = no_of_bets
         self.id = id
